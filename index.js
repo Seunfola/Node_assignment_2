449c885bfd4c4ca11687a4515ec3361b079ecaf2
Folahan O Mosunmola <seunfola1@gmail.com>
2023-03-20 12:02:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//import the sum function from the math.js module using require
+ const {sum,calculator,subtract} = require('./math.js');
+
+//  const calculator = require("./math");
+
+ //Call the sum function with two numbers and log the result to the console
+  console.log(sum(5,7));
+  //Call the multiply method on the calculator object with two numbers and log the result to the console
+
+  console.log(calculator(2, 3));
+
+console.log(subtract(14,5))
