83fd3ff7b230c62bcc1e4593213fa06a56b7c3cf
Folahan O Mosunmola <seunfola1@gmail.com>
2023-03-20 12:02:22 +0100

    Add files via upload

---
+++ math.js
@@ -0,0 +1,12 @@
+//define a function called sum that takes two arguments and returns their sum.
+
+const sum =(a,b) => a+b;
+
+const calculator =(a, b) => {
+  //Add a method to the calculator object called multiply
+  multiply: a * b
+};
+
+const subtract =(a,b)=>a-b
+//Export the sum function so that it can be used in another module
+module.exports = { sum, subtract, calculator };
